f9a31b0731543b23e99364b1e7906736b439b1af
vittoriom94 <vimattei@hotmail.it>
2017-12-03 17:59:29 +0100

    dati e giornata

--- dati.py
+++ dati.py
@@ -7,4 +7,19 @@ class DatiPartite:
     self.giornate = TreeMap()
 
   def inserisci_partita(self, dati_riga):
+    # 0 - campionato
+    # 1 - giornata
+    # 2 - data
+    # 3 - home
+    # 4 - opsite
+    # 5 - fthg
+    # 6 - ftag
+    # 7 - ftr
+    # 8 - hthg
+    # 9 - htag
+    # 10 - htr
+    g = self.giornate[dati_riga[1]]
     pass
+
+  def get_giornata(self, n_giornata):
+    return self.giornate[n_giornata]
---
+++ giornata.py
@@ -0,0 +1,29 @@
+from TdP_collections.hash_table.probe_hash_map import ProbeHashMap
+from partita import Partita
+
+
+class Giornata:
+
+
+  def __init__(self, n_giornata):
+    self.n_giornata = n_giornata
+    self.partite = ProbeHashMap()
+
+  def inserisci_partita(self, partita):
+    # 0 - campionato
+    # 1 - giornata
+    # 2 - data
+    # 3 - home
+    # 4 - opsite
+    # 5 - fthg
+    # 6 - ftag
+    # 7 - ftr
+    # 8 - hthg
+    # 9 - htag
+    # 10 - htr
+    p = Partita(partita[3],partita[4],partita[5],partita[6],partita[8],partita[9],partita[2],partita[1])
+    self.partite[partita[3]] = p
+    self.partite[partita[4]] = p
+
+  def get_partita(self, squadra):
+    return self.partite[squadra]
